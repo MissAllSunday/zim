097820b678300c21c0bb3e600985afa5e7ca752b
Jessica González <suki@missallsunday.com>
2016-12-21 19:46:56 -0600

    Make it more compact
    Signed-off-by: Jessica González <suki@missallsunday.com>

--- index.php
+++ index.php
@@ -5,10 +5,13 @@ require_once('lib/autoload.php');
 /** @var \Base $f3 */
 $f3 = \Base::instance();
 
-$f3->set('AUTOLOAD', 'app/');
-$f3->set('UI', 'app/views/');
-$f3->set('SCHEME', 'https');
-$f3->set('TZ', 'America/Mexico_City');
+$f3->mset(array(
+	'AUTOLOAD' => 'app/',
+	'UI' => 'app/views/',
+	'SCHEME' => 'https',
+	'TZ' => 'America/Mexico_City',
+));
+
 $f3->config('setup.cfg');
 $f3->set('DB', new DB\SQL(
 	''. $f3->get('_db.type') .':host='. $f3->get('_db.host') .';port='. $f3->get('_db.port') .';dbname='. $f3->get('_db.name') .'',
