f1bc96961c2b96d4d964af4e701d4447af6348dc
Jessica González <suki@missallsunday.com>
2016-12-22 20:11:03 -0600

    Adding/replacing BBC codes
    Signed-off-by: Jessica González <suki@missallsunday.com>

--- app/controllers/Blog.php
+++ app/controllers/Blog.php
@@ -2,6 +2,8 @@
 
 namespace Controllers;
 
+use Services;
+
 class Blog
 {
 	function __construct()
--- app/services/CustomCodeDefinitionSet.php
+++ app/services/CustomCodeDefinitionSet.php
@@ -3,10 +3,28 @@
 namespace Services;
 use \JBBCode;
 
-class CustomCodeDefinitionSet extends \JBBCode\DefaultCodeDefinitionSet
+class CustomCodeDefinitionSet extends \JBBCode\DefaultCodeDefinitionSet implements \JBBCode\CodeDefinitionSet
 {
 	public function __construct()
 	{
 		parent::__construct();
+
+		$urlValidator = new \JBBCode\validators\UrlValidator();
+
+		// Center tag
+		$builder = new \JBBCode\CodeDefinitionBuilder('center', '<p class="text-center">{param}</p>');
+		array_push($this->definitions, $builder->build());
+
+		// Overwrite the img tag.
+		$builder = new \JBBCode\CodeDefinitionBuilder('img', '<img class="img-responsive center-block" src="{param}" />');
+		$builder->setUseOption(false)->setParseContent(false)->setBodyValidator($urlValidator);
+
+		// Hackish... but meh
+		$this->definitions[6] = $builder->build();
+
+		// Moar hackish stuff... yay!
+		$builder = new \JBBCode\CodeDefinitionBuilder('img', '<img class="img-responsive center-block" src="{param}" alt="{option}" />');
+		$builder->setUseOption(true)->setParseContent(false)->setBodyValidator($urlValidator);
+		$this->definitions[7] = $builder->build();
 	}
 }
