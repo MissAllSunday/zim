28a040974c6a6361f555e02e0a2a2745e919c8cd
Jessica González <suki@missallsunday.com>
2016-12-28 14:23:10 -0600

    move stuff around for no reason!
    Signed-off-by: Jessica González <suki@missallsunday.com>

--- app/controllers/Blog.php
+++ app/controllers/Blog.php
@@ -8,24 +8,18 @@ class Blog
 {
 	function __construct()
 	{
-		$this->msgs = new \DB\SQL\Mapper(\Base::instance()->get('DB'),'suki_c_message');
+		$this->model = new \Models\Blog();
 	}
 
 	function home(\Base $f3, $params)
 	{
-		$limit = 10;
 		$start = $params['page'] ? $params['page'] : 0;
 
-		$f3->set('messages', $f3->get('DB')->exec('
-			SELECT m.msgTime, m.title, m.url
-			FROM suki_c_topic AS t
-			LEFT JOIN suki_c_message AS m ON (m.msgID = t.fmsgID)
-			WHERE boardID = 1
-			ORDER BY m.msgID DESC
-			LIMIT :start, :limit', array(
-			':limit' => $limit,
-			':start' => ($start * $limit)
-		)));
+		$f3->set('messages', $this->model->getEntries([
+			'limit' => 10,
+			'start' => $start,
+			'board' => 1
+		]));
 
 		$f3->set('pagination', array(
 			'next' => $start + 1,
--- app/models/Blog.php
+++ app/models/Blog.php
@@ -2,7 +2,31 @@
 
 namespace Models;
 
-class Blog extends Base
+class Blog
 {
+	function __construct()
+	{
+		$this->f3 = \Base::instance();
+		$this->mapper = new \DB\SQL\Mapper($this->f3->get('DB'),'suki_c_message');
+	}
 
+	function getComments()
+	{
+
+	}
+
+	function getEntries($params = array())
+	{
+		return $this->f3->get('DB')->exec('
+			SELECT m.msgTime, m.title, m.url
+			FROM suki_c_topic AS t
+			LEFT JOIN suki_c_message AS m ON (m.msgID = t.fmsgID)
+			WHERE boardID = :board
+			ORDER BY m.msgID DESC
+			LIMIT :start, :limit', array(
+			':limit' => $params['limit'],
+			':start' => ($params['start'] * $params['limit']),
+			':board' => $params['board'],
+		));
+	}
 }
