a9a454c774979bb7e94d5132a3e57bb67a325d14
Jessica González <suki@missallsunday.com>
2016-12-28 09:36:47 -0600

    Move the config stuff to a service I will take care of organize that stuff alter...
    Signed-off-by: Jessica González <suki@missallsunday.com>

---
+++ app/services/Config.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Services;
+
+class Config
+{
+	function __construct(\Base $f3)
+	{
+		$this->f3 = $f3;
+	}
+
+	function init()
+	{
+		$this->f3->mset(array(
+			'UI' => 'views/',
+			'SCHEME' => 'https',
+			'TZ' => 'America/Mexico_City',
+			'site' => array(
+				'title' => 'Miss All Sunday'
+			),
+		));
+
+		$this->f3->config('setup.cfg');
+		$this->f3->set('DB', new \DB\SQL(
+			''. $this->f3->get('_db.type') .':host='. $this->f3->get('_db.host') .';port='. $this->f3->get('_db.port') .';dbname='. $this->f3->get('_db.name') .'',
+			$this->f3->get('_db.user'),
+			$this->f3->get('_db.password')
+		));
+	}
+}
--- index.php
+++ index.php
@@ -1,26 +1,16 @@
 <?php
+
 // composer autoloader for required packages and dependencies
 require_once('lib/autoload.php');
 
 /** @var \Base $f3 */
 $f3 = \Base::instance();
 
-$f3->mset(array(
-	'AUTOLOAD' => 'app/',
-	'UI' => 'views/',
-	'SCHEME' => 'https',
-	'TZ' => 'America/Mexico_City',
-	'site' => array(
-		'title' => 'Miss All Sunday'
-	),
-));
+$f3->set('AUTOLOAD', 'app/');
 
-$f3->config('setup.cfg');
-$f3->set('DB', new DB\SQL(
-	''. $f3->get('_db.type') .':host='. $f3->get('_db.host') .';port='. $f3->get('_db.port') .';dbname='. $f3->get('_db.name') .'',
-	 $f3->get('_db.user'),
-	$f3->get('_db.password')
-));
+$f3->set('Config', new \Services\Config($f3));
+
+$f3->get('Config')->init();
 
 // Home and pagination.
 $f3->route(array(
