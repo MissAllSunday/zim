248fe7cfa85f1c8af8e0b091bb12958cf4a81ae6
Jessica González <suki@missallsunday.com>
2017-01-03 17:05:39 -0600

    post controller
    Signed-off-by: Jessica González <suki@missallsunday.com>

---
+++ app/controllers/Post.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Controllers;
+
+class Post extends Auth
+{
+	function __construct()
+	{
+		$this->model = new \Models\Blog(\Base::instance()->get('DB'));
+	}
+
+	function post()
+	{
+
+	}
+
+	function preview()
+	{
+
+	}
+}
