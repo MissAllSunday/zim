fbdbf8e057bfde4413498971eb9f6bbfe8b01b43
Jessica González <suki@missallsunday.com>
2017-01-03 19:26:32 -0600

    some more txt strings
    Signed-off-by: Jessica González <suki@missallsunday.com>

--- dict/en-US.php
+++ dict/en-US.php
@@ -1,11 +1,14 @@
 <?php
 
 return [
+	// General purpose
 	'page' => ' - page {0}',
 	'start' => 'Start',
 	'first' => 'First',
 	'last' => 'Last',
 	'greet' => 'Hello there {0} one!',
+
+	// Login errors
 	'error_msg' => 'There was some errors:',
 	'error_bad_token' => 'User verification failed miserably, it was probably your fault!',
 	'error_possible_bot' => 'You had harvested enough data for today boy, go get some rest',
@@ -13,18 +16,30 @@ return [
 	'error_empty_password' => 'Empty password field. Nope, your password isn\'t 1234.',
 	'error_bad_email' => 'The email you provided isn\'t valid, Dunno why.',
 	'error_no_user' => 'I am legally forced to tell you there are no records of an user ever registered here with the credentials you provided. <strong>Protip:</strong> Go back in time and register an account then come back and try again.',
+
+	// Login, logout, signup, etc.
 	'signup' => 'Not an user? mmm how about <a href="{0}">signing up?</a>',
 	'signin' => 'Sign in',
-	'logout' => 'logout',
-	'login' => 'login something',
+	'logout' => 'Logout',
+	'login' => 'Login',
 	'login_success' => 'Welcome aboard pal!',
-	'logout_success' => 'phew... are you glad is it over? yeah.. me too!',
+	'logout_success' => 'phew... are you glad its over? yeah.. me too!',
 	'login_password' => 'Password',
 	'login_email' => 'Email',
-	'submit' => 'Submit',
+
+	// Default site texts
 	'site_desc' => 'You expect me to act like a lover
 Consider my moves and deserve the reward
 To hold you in my arms and wait, wait
 Wait for something to happen...',
 	'site_keywords' => 'miss all sunday, one piece, theories, manga, capitulos, film gold, online, episodes, arcs, Suki, SMF, free, mods, php, jquery,',
+
+	// Posting.
+	'submit' => 'Submit',
+	'preview' => 'Preview',
+	'Title' => 'Title',
+
+	// Posting errors.
+	'empty_title' => 'A title is required',
+	'empty_body' => 'You kinda need to post something...',
 ];
