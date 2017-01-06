35460c6b71fa370f7d498d6543e752037b290984
Jessica González <suki@missallsunday.com>
2017-01-06 11:10:14 -0600

    topic model
    Signed-off-by: Jessica González <suki@missallsunday.com>

---
+++ app/models/Topic.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Models;
+
+class User extends \DB\SQL\Mapper
+{
+	function __construct(\DB\SQL $db)
+	{
+		parent::__construct($db, 'suki_c_topic');
+	}
+
+	public function all()
+	{
+		$this->load();
+		return $this->query;
+	}
+
+	public function getById($id = 0)
+	{
+		$this->load(array('topicID = ?', $id));
+
+		return $this->query;
+	}
+}
