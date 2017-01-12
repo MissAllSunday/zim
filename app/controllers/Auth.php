276ec0994c6abd67c9a79fe9bec92f7d5eff258e
Jessica González <suki@missallsunday.com>
2017-01-12 09:48:37 -0600

    Do the same for Auth fixes #2
    Signed-off-by: Jessica González <suki@missallsunday.com>

--- app/controllers/Auth.php
+++ app/controllers/Auth.php
@@ -4,14 +4,24 @@ namespace Controllers;
 
 class Auth
 {
+	protected $_models = [];
+	protected $_defaultModels = ['message', 'user'];
+
 	function beforeRoute($f3)
 	{
+		// Gotta stay classy...
+		foreach ($this->_defaultModels as $m)
+		{
+			// ...OK no....
+			$class = '\Models\\'. ucfirst($m);
+			$this->_models[$m] = new $class($f3->get('DB'));
+		}
+
 		if(empty($f3->get('SESSION.user')))
 			$f3->reroute('/login');
 
 		// Get current user data.
-		$this->userModel = new \Models\User($f3->get('DB'));
-		$f3->set('currentUser', $this->userModel->load(array('userID' => $f3->get('SESSION.user'))));
-		$this->userModel->reset();
+		$f3->set('currentUser', $this->_models['user']->load(array('userID' => $f3->get('SESSION.user'))));
+		$this->_models['user']->reset();
 	}
 }
