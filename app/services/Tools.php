13193594c5c2f1d8b0e5c1563012500ed18e829a
Jessica González <suki@missallsunday.com>
2017-02-06 15:38:49 -0600

    A bunch of random lines
    Signed-off-by: Jessica González <suki@missallsunday.com>

--- app/services/Tools.php
+++ app/services/Tools.php
@@ -126,7 +126,7 @@ class Tools extends \Prefab
 
 	function metaDescription($str = '', $limit = 150)
 	{
-		$str = trim(preg_replace('/ +/', ' ', preg_replace('/[^A-Za-z0-9 ]/', ' ', urldecode(html_entity_decode(strip_tags($str), ENT_NOQUOTES)))));
+		$str = trim(preg_replace('/ +/', ' ', preg_replace('/[^A-Za-z0-9 ]/', ' ', urldecode(html_entity_decode(strip_tags(str_replace('&#39', '\'', $str)), ENT_NOQUOTES)))));
 
 		if (strlen ($str) > $limit)
 		{
