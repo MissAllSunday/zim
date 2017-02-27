83251ee0d6eab7911caec2ef245e6302678487bd
Jessica González <suki@missallsunday.com>
2017-02-27 08:38:59 -0600

    fix the signup thing
    Signed-off-by: Jessica González <suki@missallsunday.com>

--- views/js/signup.js
+++ views/js/signup.js
@@ -3,16 +3,16 @@ $(function() {
 	var _urlField = $('<input/>').attr({
 		type: 'text',
 		name: 'data[avatar]',
-		class: 'hidden',
+		class: 'form-control  hidden',
 		id: 'url_field'
 	}).insertAfter('#radio_url');
-	$("input[name='data[avatarType]']:radio").on('change', function()
+	$("input[name='data[avatarType]']").on('change', function()
 	{
 		if (this.value == 'url') {
-			$('#url_field').removeClass('hidden').addCalss('show');
+			$('#url_field').removeClass('hidden').addClass('show');
 		}
 		else {
-			$('#url_field').removeClass('show').addCalss('hidden');
+			$('#url_field').removeClass('show').addClass('hidden');
 		}
 	});
 });
