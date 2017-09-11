603c4884beac3cf0844a5e0a893154474d71f2df
Jessica González <suki@missallsunday.com>
2017-09-10 20:24:38 -0500

    mock db call
    Signed-off-by: Jessica González <suki@missallsunday.com>

--- bootstrap-test.php
+++ bootstrap-test.php
@@ -6,17 +6,14 @@ define('ROOT', __DIR__);
 require_once('lib/autoload.php');
 
 $f3 = \Base::instance();
-$f3->config('setup.ini');
 $f3->set('AUTOLOAD', 'app/');
 $f3->set('DB', new \DB\SQL(
-	$f3->get('_db.type') .':host='. $f3->get('_db.host') .';port='. $f3->get('_db.port') .';dbname='. $f3->get('_db.name') .'',
+	"mysql:host=localhost;port="";dbname=test_zim",
 	"root",
 	"",
 	[\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4;']
 ));
-$f3->set('USER', new \DB\SQL\Session($f3->get('DB'), $f3->get('_db.prefix') . 'ses', true, function($session){
-		return true;
-	}));
+$f3->set('USER', []);
 $f3->set('QUIET', true);
 $f3->set('ONERROR',function(){});
 
