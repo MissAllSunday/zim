fafef50e9f3364f655ddba7ca95d0b1780793035
Jessica González <suki@missallsunday.com>
2017-09-10 20:38:43 -0500

    mock some moar stuff
    Signed-off-by: Jessica González <suki@missallsunday.com>

--- bootstrap-test.php
+++ bootstrap-test.php
@@ -7,10 +7,17 @@ require_once('lib/autoload.php');
 
 $f3 = \Base::instance();
 $f3->set('AUTOLOAD', 'app/');
+$f3->set("_db.prefix", "suki_c_");
+$f3->set("paginationLimit", 10);
+$f3->set("site", [
+	"home" => "Miss All Sunday",
+	"title" => "Miss All Sunday",
+	"metaTitle" => "Miss All Sunday - ",
+]);
 $f3->set('DB', new \DB\SQL(
-	"mysql:host=localhost;port="";dbname=test_zim",
-	"root",
-	"",
+	'mysql:host=localhost;port="";dbname=test_zim',
+	'root',
+	'',
 	[\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4;']
 ));
 $f3->set('USER', []);
